0ad26b27f928cb5c9b07c6324f7aea0a05ead3ae
Filip Tarajko <filiptarajko@gmail.com>
2024-01-20 03:52:22 +0100

    Add a simple camera rotation system

--- Player.gd
+++ Player.gd
@@ -30,7 +30,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
-	input_dir = input_dir.rotated(camera_stick.rotation.x)
+	input_dir = input_dir.rotated(-camera_stick.rotation.y)
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -38,6 +38,9 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	var mouse_velocity = Input.get_last_mouse_velocity()
+	camera_stick.rotate_y(-mouse_velocity.x*X_SENSITIVITY)
+	camera_stick.rotate_object_local(Vector3.RIGHT, -mouse_velocity.y*Y_SENSITIVITY)
 
 
 	move_and_slide()
