c4ee86b43276d8d33fb4315abac919cfb255e665
Filip Tarajko <filiptarajko@gmail.com>
2024-01-21 22:14:22 +0100

    Prepare placeholder map generation for improvements

--- World.gd
+++ World.gd
@@ -3,8 +3,9 @@ extends Node3D
 @export var game: Node3D
 @export var blocks: Node3D
 @export var rigidbody_script: Script
-var platform_range = 8
-var platform_depth = 16
+var platform_lenght = 17
+var platform_depth = 17
+
 
 func place_block(new_block_position, index):
 	var new_block = game.blocks[index].duplicate(false)
@@ -15,21 +16,25 @@ func place_block(new_block_position, index):
 	blocks.add_child(new_block)
 	return new_block
 
+
 func place_obtainable_block(new_block_position):
 	var placed_block = place_block(new_block_position, game.obtainable_blocks_indexes.pick_random())
 	if placed_block is RigidBody3D:
 		placed_block.freeze = false
 
+
 func place_commonly_spawned_block(new_block_position):
 	place_block(new_block_position, game.commonly_spawned_blocks_indexes.pick_random())
+
 
 func place_bedrock(new_block_position):
 	place_block(new_block_position, game.name_to_index_dictionary["bedrock"])
 
+
 func generate_terrain():
-	print("volume: "+str((2*platform_range+1)*(2*platform_range+1)*platform_depth))
-	for i in range(2*platform_range+1):
-		for j in range(2*platform_range+1):
-			for k in range(platform_depth):
-				place_commonly_spawned_block(Vector3(i-platform_range, -k, j-platform_range))
-			place_bedrock(Vector3(i-platform_range, -platform_depth, j-platform_range))
+	print("volume: "+str((platform_lenght)*(platform_lenght)*(platform_depth-1)))
+	for i in range(platform_lenght):
+		for j in range(platform_lenght):
+			for k in range(platform_depth-1):
+				place_commonly_spawned_block(Vector3(i, -k, j))
+			place_bedrock(Vector3(i, -platform_depth, j))
