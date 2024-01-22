c71c20e77cff03cce06fca7db3201e27d615699c
Filip Tarajko <filiptarajko@gmail.com>
2024-01-22 11:58:29 +0100

    Fix falling blocks not freezing when stacked

--- blocks/rigidbody_block.gd
+++ blocks/rigidbody_block.gd
@@ -7,9 +7,14 @@ var world
 var block_index: int
 
 
+func is_nearly_zero(num):
+	return num < 0.001 && num > -0.001 
+
+
 func _physics_process(_delta):
 	if not freeze:
-		if last_velocity == 0 && linear_velocity.y == 0:
+		print(linear_velocity.y)
+		if is_nearly_zero(last_velocity) && is_nearly_zero(linear_velocity.y):
 			position.y = round(position.y)
 			freeze = true
 			world.handle_block_appeared(position, block_index)
